140729597c984f63f365380df80d7250c69c5178
amits1995 <amit.shani1995@gmail.com>
2015-12-02 11:09:37 +0200

    Update app.js

--- Scripts/custom-scripts/app.js
+++ Scripts/custom-scripts/app.js
@@ -38,11 +38,11 @@ app.value('default_options', default_options);
 
 app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/Json/', {
-        templateUrl: '/Views/JsonOnly.html',
+        templateUrl: 'Views/JsonOnly.html',
         controller: 'singleEditorCtrl',
         controllerAs: 'vm'
     }).otherwise({
-        templateUrl: '/Views/MainPage.html',
+        templateUrl: 'Views/MainPage.html',
         controller: 'mainCtrl',
         controllerAs: 'ctrl'
     });
